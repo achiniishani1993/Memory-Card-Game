63e13bf2b675a75627d008dadb1a61beb248955e
achiniishani1993 <achiniishani1993@gmail.com>
2026-04-20 10:35:27 +0200

    replaced clicked function

--- script/index.js
+++ script/index.js
@@ -1,20 +1,18 @@
-//define the array 8 elements 
-//shuffle function 
+//define the array 8 elements
+//shuffle function
 // display in UI shuffle elements
 //click twice - match cards score need to add and turns not turn back if not cards turn back
-// All cards matched - congratulations 
-
+// All cards matched - congratulations
 
 const gameContainer = document.getElementById("game-container");
 const endGameContainer = document.getElementById("end-game-container");
 const scoreEl = document.getElementById("score");
 
-
 let cardsArray = ["A", "A", "B", "B", "C", "C", "D", "D"];
 let shuffleCards = [];
 let firstCard = null;
 let secondCard = null;
-
+let score = 0;
 
 function shuffle(array) {
   let i = array.length,
@@ -23,35 +21,66 @@ function shuffle(array) {
   while (--i > 0) {
     j = Math.floor(Math.random() * (i + 1)); //5
     console.log("j " + j); // 5
-    temp = cardsArray[j]; // c,  
-    console.log("cardsarray j " + cardsArray[j])
-    console.log("cardsarray i " + cardsArray[i])
+    temp = cardsArray[j]; // c,
+    console.log("cardsarray j " + cardsArray[j]);
+    console.log("cardsarray i " + cardsArray[i]);
     array[j] = array[i];
     array[i] = temp;
   }
-  console.log(array) 
- return array;
+  console.log(array);
+  return array;
 }
 
-function createBoard(){
-   let shuffleCards = shuffle(cardsArray);
-    console.log(shuffleCards);
+function createBoard() {
+  let shuffleCards = shuffle(cardsArray);
+  console.log(shuffleCards);
 
- for (let i=0; i<cardsArray.length; i++) {
+  for (let i = 0; i < cardsArray.length; i++) {
     let card = document.createElement("div");
     card.dataset.letter = shuffleCards[i];
     card.innerHTML = "?";
     gameContainer.appendChild(card);
-    card.addEventListener('click', function() {
-     card.innerHTML = card.dataset.letter;
-     let cardloard = card.innerHTML
-        console.log(cardloard);
-    });
-}
 
+    let lockBoard = false;
+
+    card.addEventListener("click", function () {
+      if (lockBoard) return;
+      if (card === firstCard) return;
+
+      card.innerHTML = this.dataset.letter;
+
+      if (firstCard === null) {
+        firstCard = card;
+      } else if (secondCard === null) {
+        secondCard = card;
+        lockBoard = true;
+
+        if (firstCard.dataset.letter === secondCard.dataset.letter) {
+          scoreEl.innerHTML = score += 1;
+
+          endGameContainer.style.display = "block";
+          endGameContainer.innerHTML = "Matched.. keep going";
+
+          firstCard = null;
+          secondCard = null;
+          lockBoard = false;
+        } else {
+          endGameContainer.style.display = "block";
+          endGameContainer.innerHTML = "UnMatched.. try again";
+
+          setTimeout(() => {
+            firstCard.innerHTML = "?";
+            secondCard.innerHTML = "?";
+            endGameContainer.style.display = "none";
+
+            firstCard = null;
+            secondCard = null;
+            lockBoard = false;
+          }, 1000);
+        }
+      }
+    });
+  }
 }
 
 createBoard();
-
-
-
